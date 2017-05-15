506b00ce9e226215d3204ebaab361c287726d568
Valters Mednis <valters.mednis@gmail.com>
2017-05-15 15:46:11 +0300

    Prepare for github.

--- tgbot.go
+++ tgbot.go
@@ -6,14 +6,14 @@ import "fmt"
 import "tgbot/methods"
 
 func main() {
-	mednisBot := bot.Bot{APIKey: "320490619:AAGd1-Wtx-BMciXNxAV7gahNry8Wpzy8Guk"}
+    mednisBot := bot.Bot{APIKey: "APIkey:here"}
 
 	mednisBot.OnMessage = func(msg *tgtype.Message) {
 		fmt.Printf("Recieved message \"%v\" from user with name %v\n", msg.Text, msg.From.FirstName)
 		fmt.Println(msg.Chat)
 		method := methods.SendMessage{
 			ChatID:           msg.Chat.ID,
-			Text:             "STFU",
+			Text:             "Hello world!",
 			ReplyToMessageID: msg.MessageID,
 		}
 		method.CallMethod(mednisBot.GetBotURL())
